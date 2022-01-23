bbfa107458efef6d702525f7b9ca939c3930643d
MOHAMED SAFI <safiizz24@gmail.com>
2022-01-23 00:59:52 -0800

    program to get input from console and if/else check

---
+++ ruby_basics/sample.rb
@@ -0,0 +1,13 @@
+class Sample
+#getting input from keyboard
+num = gets.chomp
+#convert to integer
+num = num.to_i;
+x = 10
+if num > x
+puts "The number is greater than x"
+else
+puts "The number is lesser than x"
+end
+end 
+
