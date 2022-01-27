3158563281fce53090704e556d20a26a92cf9c6e
MOHAMED SAFI <safiizz24@gmail.com>
2022-01-27 00:55:57 -0800

    Number Datatype intro

---
+++ ruby_basics/DataTypeEx.rb
@@ -0,0 +1,11 @@
+class DataTypeEx
+#floating point datatype representation
+cost = 12.20
+#integer datatype representation
+quanity = 50
+#multiplying cost and quantity
+total_cost = cost*quanity
+puts "The total cost of the product is #{total_cost}"
+no_of_product = total_cost/cost
+puts "No of products #{no_of_product}"
+end
