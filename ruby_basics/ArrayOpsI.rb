895e2f64f4035389fa8fe512b7216f7fd82b2f6d
MOHAMED SAFI <safiizz24@gmail.com>
2022-01-28 06:44:23 -0800

    Array iterations types

---
+++ ruby_basics/ArrayOpsI.rb
@@ -0,0 +1,15 @@
+class ArrayOpsI
+  #nums = Arrays.new
+  #puts nums
+  nums = [31,20,44,89,11]
+  N = nums.length
+  puts
+  for i in 0...N
+    puts "#{nums[i]} is in index #{i}"
+  end
+  print "-----------------------------"
+  puts
+  for i in nums
+    puts "#{i}"
+  end
+end
